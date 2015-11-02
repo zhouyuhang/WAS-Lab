88d27b73e05307f529e4c5b7ec911b658892ffbe
zhouyuhang <zhouyuhangcn@hotmail.com>
2015-11-03 00:41:44 +0800

    Bypass client side control
    Implement 2 labs of Bypass client side control

---
+++ scripts/php/clientside_1.php
@@ -0,0 +1,19 @@
+<?php
+include 'DbClass.php';
+$db = new Db();
+$quantity = $_POST['quantity'];
+$price = $_POST['price'];
+
+$result = $db -> query("UPDATE store SET product_number=product_number-$quantity where product_name='iphone6s'");
+if($db->error() != '')
+{
+    $error = $db->error();
+    $db -> close();
+    echo json_encode($error);
+}
+else
+{
+     $db -> close();
+    echo json_encode('$result');
+}
+?>
